95e300af209dd9893cde462660e6cfc6b7c9cc08
Jon Gossett <63742516+jogosset@users.noreply.github.com>
2026-06-02 18:01:53 -0500

    Refactor loadPage and remove loadLazy function
    Refactor loadPage function and remove loadLazy function.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -199,7 +199,7 @@ function loadDelayed() {
   // load anything that can be postponed to the latest here
 }
 
-export async function loadPage() {
+async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
@@ -214,35 +214,6 @@ export async function loadPage() {
 
 ...
 
-/**
- * Loads everything that doesn't need to be delayed.
- * @param {Element} doc The container element
- */
-async function loadLazy(doc) {
-
-  ...
-
-  const loadQuickEdit = async (...args) => {
-    // eslint-disable-next-line import/no-cycle
-    const { default: initQuickEdit } = await import('../tools/quick-edit/quick-edit.js');
-    initQuickEdit(...args);
-  };
-
-  const addSidekickListeners = (sk) => {
-    sk.addEventListener('custom:quick-edit', loadQuickEdit);
-  };
-
-  const sk = document.querySelector('aem-sidekick');
-  if (sk) {
-    addSidekickListeners(sk);
-  } else {
-    // wait for sidekick to be loaded
-    document.addEventListener('sidekick-ready', () => {
-    // sidekick now loaded
-      addSidekickListeners(document.querySelector('aem-sidekick'));
-    }, { once: true });
-  }
-}
 
 (() => {
   const hasQE = new URL(window.location.href).searchParams.has('quick-edit');
